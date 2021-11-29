59eea60699e58ba5da0981cb875ec78c26a70c77
JohanneAntoine <48861458+JohanneAntoine@users.noreply.github.com>
2021-11-28 23:03:47 -0500

    Delete PS1.P1.js

--- PS1.P1.js
+++
@@ -1,9 +0,0 @@
-const reverseAlpha = str => {
-    lst = str.split('');
-    console.log(lst)
-    lst2 = lst.sort().reverse();
-    return lst2.join('');
-}
-const testString = 'supercalifragilisticexpialidocious';
-
-console.log(`${testString} in reverse alphabetical order is ${reverseAlpha(testString)}`);
